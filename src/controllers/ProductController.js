68d8540d8d218bcf00123df6239df797ad8eebe3
AewInformatica <aewinformatica@gmail.com>
2019-11-03 01:58:43 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -9,11 +9,31 @@ module.exports = {
 
     return res.json(products);
   },
+
+  async show(req,res){
+    //http://localhost:3001/api/products/5dbe56ad756b061dd8f7a29d
+    const product = await Product.findById(req.params.id)
+
+    return res.json(product);
+  },
   async store(req,res){
     //recebe o corpo da requisição
     const product = Product.create(req.body); 
 
     //devolve como resposta o product criado
     return res.json(product);
+  },
+  async update(req,res){
+    //Busca o produto e atualiza e  o new = true diz que e pra retornar o valor atualizado caso false ele retorna o valor antigo
+    const product = Product.findByIdAndUpdate(req.params.id,req.body,{new: true});
+    //retornando o produto em json
+    return res.json(product); 
+
+  },
+  async destroy(req,res){
+    await Product.findByIdAndRemove(req.params.id);
+
+    //resposta de sucesso sem nenhum conteudo
+    return res.send();
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,15 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
+//listando
 routes.get('/products', ProductController.index);
+//pesquisando
+routes.get('/products/:id',ProductController.show);
+//armazenando
 routes.post('/products', ProductController.store);
+//atualizando
+routes.put('/products/:id', ProductController.update);
+//deletando
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
