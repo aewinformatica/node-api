0a0edae0279134fef3939ec1de785ad6c4efe81e
aewinformatica <aewinformatica@gmail.com>
2021-02-20 14:46:35 -0300

    Atualizacao do Projeto para hospedar API

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const cors = require ('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const requireDir = require('require-dir');
 // iniciando o APP
 const app = express();
+const port = process.env.port || "3001";
 //habilitando uso de Json na API
 app.use(express.json());
 //usando cors sem filtrar dominios liberado para todos
-app.use(cors())
+app.use(cors());
+app.use(morgan("dev"));
 
 // Iniciando o DB
-mongoose.connect('mongodb://192.168.99.100:27017/nodeapi',
+mongoose.connect('mongodb://localhost:27017/nodeapi',
 { useUnifiedTopology: true ,
   useNewUrlParser: true});
 
-
  requireDir('./src/models'); 
-
-//  const Product =  mongoose.model("Product");
-
-
 //rotas
-//use = widecard
+//use = wildcard - curinga
 app.use('/api',require("./src/routes"));
 
-app.listen(3001);
+app.listen(port,()=>{
+  console.log(`API rodando na porta ${port}`)
+});
